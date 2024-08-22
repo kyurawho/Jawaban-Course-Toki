1f56a5479f08f1eaa1796f70dd090c998414c0fb
Jeremy Christopher <michryan1134@gmail.com>
2024-08-22 21:32:00 +0700

    kompetitif 3I

---
+++ Course-kompetitif/03/I.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define fi first
+#define se second
+#define pb push_back
+#define pq priority_queue
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define Repp(i, l, r, a) for (int i = l; i < r; i += a)
+#define Rep(i, l, r) Repp(i, l, r, 1)
+#define For(i, n) Rep(i, 0, n)
+#define Fori(i, n) Rep(i, 1, n + 1)
+#define Each(x, v) for (auto x : v)
+#define Repd(i, l, r) for (int i = r - 1; i >= l; i--)
+#define Ford(i, n) Repd(i, 0, n)
+#define endl "\n"
+
+typedef pair <int, int> pii;
+typedef pair <int, pii> piii;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	int n, x;
+	cin >> n >> x;
+	int a[n];
+	For(i, n){
+		cin >> a[i];
+	}
+	
+	sort(a, a + n);
+	
+	cout << a[x - 1]<< endl;
+}
