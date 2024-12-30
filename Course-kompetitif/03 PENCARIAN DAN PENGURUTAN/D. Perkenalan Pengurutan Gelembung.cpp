13c1306f3b1a167d678c601571fe17d7048e4871
Jeremy Christopher <michryan1134@gmail.com>
2024-12-30 21:07:29 +0700

    kompetitif 3D Bubble sort - Pencarian dan Pengurutan

---
+++ Course-kompetitif/03 PENCARIAN DAN PENGURUTAN/D. Perkenalan Pengurutan Gelembung.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define fi first
+#define se second
+#define pb push_back
+#define pq priority_queue
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define Repp(i, l, r, a) for (int i = l; i < r; i += a)
+#define Rep(i, l, r) Repp(i, l, r, 1)
+#define For(i, n) Rep(i, 0, n)
+#define Fori(i, n) Rep(i, 1, n + 1)
+#define Each(x, v) for (auto x : v)
+#define Repd(i, l, r) for (int i = r - 1; i >= l; i--)
+#define Ford(i, n) Repd(i, 0, n)
+#define endl "\n"
+
+typedef pair <int, int> pii;
+typedef pair <int, pii> piii;
+
+int bubbleSort(int arr[], int n){
+    int i, j;
+    bool swapped;
+    int count = 0;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+                count++;
+            }
+        }
+       
+        if (swapped == false){
+            break;
+        }
+    }
+    return count;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	int t;
+	cin >> t;
+	int arr[t];
+	For(i, t){
+		cin >> arr[i];
+	}
+	cout << bubbleSort(arr, t);
+}
